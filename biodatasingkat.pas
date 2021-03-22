99be54fbfbc58fba5a7fd708ec9e2dc55b71d8bb
aidil rizki alfatilani <73513298+AidilRizkiAlfatilani@users.noreply.github.com>
2021-03-22 08:52:22 +0700

    tugas 1

---
+++ biodatasingkat.pas
@@ -0,0 +1,16 @@
+program biodatasingkat;
+uses crt;
+var
+
+
+begin
+writeln('biodata');
+writeln('nama   :aidil rizki alfatilani');
+writeln('NPM    :20.14.1.0026');
+writeln('alamat :desa sadawangi kecamatan lemahsugih');
+writeln('TTL    : Majalengka 13 maret 2002');
+writeln('no hp  : 085156544783');
+writeln('motto  :jalani,nikmati,syukuri');
+
+readln;
+end;
