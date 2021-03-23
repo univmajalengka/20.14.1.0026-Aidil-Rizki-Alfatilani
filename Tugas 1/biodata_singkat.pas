411815238749215d352d4c620ff89b95c89a9bbb
aidil rizki alfatilani <73513298+AidilRizkiAlfatilani@users.noreply.github.com>
2021-03-23 09:12:13 +0700

    Create biodata_singkat.pas

---
+++ Tugas 1/biodata_singkat.pas
@@ -0,0 +1,18 @@
+{Nama           : Aidil Rizki Alfatilani}
+{NPM            : 20.14.1.0026}
+{Nama program   : biodata singkat}
+
+program biodata_singkat;
+uses crt;
+var
+nama,NPM,alamat,nohp,TTL,motto :string;
+begin
+writeln('nama: Aidil Rizki Alfatilani');readln;
+writeln('NPM:20.14.1.0026');readln;
+writeln('alamat:Desa sadawangi kecamatan lemahsugih');readln;
+writeln('TTL:Majalengka,13,maret,2002');readln;
+writeln('no hp:085156544783');readln;
+writeln('motto:jalani,nikmati,syukuri');readln;
+
+readln;
+end.
